abdc13a3e66df7aa928f0d0818b2b9b9e484e241
Gunjan Mirchandani <gmirchandani5@gmail.com>
2026-09-08 10:40:56 +0530

    Complete CRUD API's for blog banckend

--- main.py
+++ main.py
@@ -157,6 +157,113 @@ def create_like(like:like):
         "id":like_id,
         **data[like_id]
     }
+@app.put('/user/{user_id}')
+def update_user(user_id: str, user: User):
+    data = user_data()
+
+    if user_id not in data:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    data[user_id] = user.model_dump(exclude=['id'])
+
+    save_user(data)
+
+    return {
+        "id": user_id,
+        **data[user_id]
+    }
+
+@app.delete('/user/{user_id}')
+def delete_user(user_id: str):
+    data = user_data()
+
+    if user_id not in data:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    del data[user_id]
+
+    save_user(data)
+
+    return {
+        "Message": "User deleted successfully"
+    }
+
+@app.put('/post/{post_id}')
+def update_post(post_id: str, post: post):
+    data = post_data()
+
+    if post_id not in data:
+        raise HTTPException(status_code=404, detail="Post not found")
+
+    data[post_id] = post.model_dump(exclude=['id'])
+
+    save_post(data)
+
+    return {
+        "id": post_id,
+        **data[post_id]
+    }
+
+@app.delete('/post/{post_id}')
+def delete_post(post_id: str):
+    data = post_data()
+
+    if post_id not in data:
+        raise HTTPException(status_code=404, detail="Post not found")
+
+    del data[post_id]
+
+    save_post(data)
+
+    return {
+        "Message": "Post deleted successfully"
+    }
+
+@app.put('/comments/{comment_id}')
+def update_comment(comment_id: str, comment: comment):
+    data = comment_data()
+
+    if comment_id not in data:
+        raise HTTPException(status_code=404, detail="Comment not found")
+
+    data[comment_id] = comment.model_dump(exclude=['id'])
+
+    save_comment(data)
+
+    return {
+        "id": comment_id,
+        **data[comment_id]
+    }
+
+@app.delete('/comments/{comment_id}')
+def delete_comment(comment_id: str):
+    data = comment_data()
+
+    if comment_id not in data:
+        raise HTTPException(status_code=404, detail="Comment not found")
+
+    del data[comment_id]
+
+    save_comment(data)
+
+    return {
+        "Message": "Comment deleted successfully"
+    }
+
+@app.delete('/likes/{like_id}')
+def delete_like(like_id: str):
+    data = like_data()
+
+    if like_id not in data:
+        raise HTTPException(status_code=404, detail="Like not found")
+
+    del data[like_id]
+
+    save_like(data)
+
+    return {
+        "Message": "Like deleted successfully"
+    }
 
 
 
